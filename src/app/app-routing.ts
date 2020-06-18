e9562794bbab8ef647235092f0b9f0be65adf478
joseayebenes <joseayebenes@gmail.com>
2020-06-18 20:38:38 +0200

    Video 35

---
+++ src/app/app-routing.ts
@@ -0,0 +1,13 @@
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NopagefoundComponent }
+];
+
+export const AppRoutes = RouterModule.forRoot(routes, { useHash: true });
